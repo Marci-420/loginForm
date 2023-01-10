126d3750cdb33a21bb3a6d8b9b9461a8baf124e0
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-01-10 10:47:28 +0100

    checkbox érzékelés javítva

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 function login(){
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
-    let isTicked = document.getElementById("box").value
+    let isTicked = document.getElementById("box").checked
+
+    console.log(isTicked)
 
     if (username == "admin" && password == "admin1234" && isTicked){
         alert("üdv admin")
